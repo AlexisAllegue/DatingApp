42d6dc108e595d2c6f431e88655220f5ca9d2a0b
qallegue <qiuyi.allegue@gmail.com>
2025-06-22 19:46:48 -0400

    End of section 7

--- API/Program.cs
+++ API/Program.cs
@@ -1,7 +1,8 @@
 using API.Interfaces;
 using API.Services;
 using API.Extensions;
-
+using API.Middleware;
+using API.Errors;
 var builder = WebApplication.CreateBuilder(args);
 
 // Middleware
@@ -14,6 +15,7 @@ builder.Services.AddApplicationAuth(builder.Configuration);
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+app.UseMiddleware<ExceptionMiddleware>();
 app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200", "https://localhost:4200"));
 app.UseAuthentication();
 app.UseAuthorization();
